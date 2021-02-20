f7203d965ca9fe5d3bbbdce3b9ea22ef3d4a33cb
LionKingSmasher <38488586+LionKingSmasher@users.noreply.github.com>
2021-02-20 20:57:29 +0900

    haskell today
    read file program

---
+++ 20210220/1.hs
@@ -0,0 +1,28 @@
+import System.IO(
+	Handle,
+	FilePath,
+	IOMode(ReadMode),
+	openFile,
+	hGetLine,
+	hPutStr,
+	hClose,
+	hIsEOF,
+	stderr)
+
+import Control.Monad(when)
+
+dumpFile :: Handle -> FilePath -> Integer -> IO ()
+dumpFile handle filename lineNumber = do
+	end <- hIsEOF handle
+	when(not end) $ do
+		line <- hGetLine handle
+		putStrLn $ filename ++ ":" ++ show lineNumber ++ ": " ++ line
+		dumpFile handle filename $ lineNumber + 1
+
+main :: IO ()
+main = do
+	hPutStr stderr "Filename: "
+	filename <- getLine 
+	handle <- openFile filename ReadMode
+	dumpFile handle filename 1
+	hClose handle
